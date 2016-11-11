62ae75cd980c8bbdef756b90a570212b127e93fe
Noah van Bochove <noahlvb@gmail.com>
2016-11-11 16:26:47 +0100

    Fixed the relative directory for the uploads

--- app/webserver/router-back/router-prints.js
+++ app/webserver/router-back/router-prints.js
@@ -50,7 +50,7 @@ router.post('/add', account.isLoggedInAsUser, function(req, res){
         P_platformAdhesionType: req.body.P_platformAdhesionType
     };
 
-    fs.rename(req.file.path, 'files/slt/' + req.file.filename + '.stl', function(err){
+    fs.rename('./' + req.file.path, './files/slt/' + req.file.filename + '.stl', function(err){
         if(err){
             logger.error(err);
             req.flash('error', 'Uploaden mislukt');
