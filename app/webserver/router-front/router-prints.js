e19b0db560f4ddb7a4419dbb03f4da8d1f07fd60
Noah van Bochove <noahlvb@gmail.com>
2016-11-29 09:46:53 +0100

    Added the username in the print item page for the reviewers

--- app/webserver/router-front/router-prints.js
+++ app/webserver/router-front/router-prints.js
@@ -3,6 +3,7 @@ var request = require("request");
 
 var account = require("./../../account");
 var printsDB = require("./../../../models/prints");
+var usersDB = require("./../../../models/users");
 var settings = require("./../../../config/settings");
 
 Number.prototype.toFixedDown = function(digits) {
@@ -34,39 +35,43 @@ router.get('/archived', account.isLoggedInAsUser, function(req, res){
 });
 
 router.get('/:id', account.isLoggedInAsUser, function(req, res){
-    printsDB.findOne({_id: req.params.id}, function(err, document){
-        res.set({"Content-Type": "text/html"});
-        if(req.user.type == "admin" || req.user.type == "supervisor" || req.user._id == document.owner){
-            request.get({
-                url: settings.octo_addr + 'api/job',
-                headers: {'X-Api-Key': settings.octo_key},
-                json: true
-            }, function(err, response, body){
-                if (err) logger.error(err);
-                if (body && body.progress.completion != null){
-                    res.render('prints-item', {
-                        user : {
-                            username : req.user.username,
-                            type : req.user.type
-                        },
-                        print : document,
-                        progress : body.progress.completion.toFixedDown(2)
-                    });
-                }else{
-                    res.render('prints-item', {
-                        user : {
-                            username : req.user.username,
-                            type : req.user.type
-                        },
-                        print : document,
-                        progress : 0
-                    });
-                }
-            });
-        }else{
-            req.flash('error', 'U hebt geen permissie om deze print opdracht te bekijken');
-            res.redirect('/');
-        }
+    printsDB.findOne({_id: req.params.id}, function(err, documentPrint){
+        usersDB.findOne({_id: documentPrint.owner}, function(err, documentUser){
+            res.set({"Content-Type": "text/html"});
+            if(req.user.type == "admin" || req.user.type == "supervisor" || req.user._id == documentPrint.owner){
+                request.get({
+                    url: settings.octo_addr + 'api/job',
+                    headers: {'X-Api-Key': settings.octo_key},
+                    json: true
+                }, function(err, response, body){
+                    if (err) logger.error(err);
+                    if (body && body.progress.completion != null){
+                        res.render('prints-item', {
+                            user : {
+                                username : req.user.username,
+                                type : req.user.type
+                            },
+                            print : documentPrint,
+                            printOwner: documentUser.username,
+                            progress : body.progress.completion.toFixedDown(2)
+                        });
+                    }else{
+                        res.render('prints-item', {
+                            user : {
+                                username : req.user.username,
+                                type : req.user.type
+                            },
+                            print : documentPrint,
+                            printOwner: documentUser.username,
+                            progress : 0
+                        });
+                    }
+                });
+            }else{
+                req.flash('error', 'U hebt geen permissie om deze print opdracht te bekijken');
+                res.redirect('/');
+            }
+        });
     });
 });
 
